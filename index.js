a3d78ee4ab23a155642b703fb4bdc72186690768
Carl Dungca <carl.dungca@yahoo.com>
2024-04-01 23:48:05 -0700

    JSX converted to plain JS

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-// Task: Turn React into plain JS
+const element = {
+  type: 'h1',
+  props: {
+    title: 'foo',
+    children: 'hello',
+  },
+};
 
-const element = <h1 title="foo">Hello</h1>;
 const container = document.getElementById('root');
-ReactDOM.render(element, container);
+
+const node = document.createElement(element.type);
+node['title'] = element.props.title;
+
+const text = document.createTextNode('');
+text['nodeValue'] = element.props.children;
+
+node.appendChild(text);
+container.appendChild(node);
