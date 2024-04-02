8dd90fd4a63db5aa331a1e58fd540f3eada58796
Carl Dungca <carl.dungca@yahoo.com>
2024-04-02 00:31:21 -0700

    Barebones rendering library

--- index.js
+++ index.js
@@ -1,18 +1,59 @@
-const element = {
-  type: 'h1',
-  props: {
-    title: 'foo',
-    children: 'hello',
-  },
+// Takes transpiled JSX and turns it into a React object which will be rendered on the screen
+function createElement(type, props, ...children) {
+  console.log(props, children);
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map(child =>
+        typeof child === 'object' ? child : createTextElement(child)
+      ),
+    },
+  };
+}
+
+function createTextElement(text) {
+  return {
+    type: 'TEXT_ELEMENT',
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+}
+
+// This is our library
+const Didact = {
+  createElement,
+  render,
 };
 
+const element = Didact.createElement(
+  'div',
+  { id: 'foo' },
+  Didact.createElement('a', null, 'bar'),
+  Didact.createElement('b')
+);
+
+function render(element, container) {
+  // TODO: Create DOM nodes
+  const dom =
+    element.type == 'TEXT_ELEMENT'
+      ? document.createTextNode('')
+      : document.createElement(element.type);
+
+  const isProperty = key => key !== 'children';
+
+  Object.keys(element.props)
+    .filter(isProperty)
+    .forEach(name => {
+      dom[name] = element.props[name];
+    });
+
+  element.props.children.forEach(child => render(child, dom));
+
+  container.appendChild(dom);
+}
+
 const container = document.getElementById('root');
-
-const node = document.createElement(element.type);
-node['title'] = element.props.title;
-
-const text = document.createTextNode('');
-text['nodeValue'] = element.props.children;
-
-node.appendChild(text);
-container.appendChild(node);
+Didact.render(element, container);
